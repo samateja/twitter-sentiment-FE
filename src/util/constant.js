207643a5254829f1d3d9eb7bcea4ca764417b17c
Venkata <venkatas@moback.com>
2018-11-04 15:58:32 +0530

    latest buid deployed

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  'endpoint' : 'https://http://ec2-54-211-223-223.compute-1.amazonaws.com:80',
+  'endpoint' : 'https://http://ec2-54-211-223-223.compute-1.amazonaws.com:8080',
   //'endpoint' : 'http://localhost:3001',
     'production': false,
     'environment': 'dev'   //change this variable accordingly when you change endpoint(dev,qa,stage and prod).
